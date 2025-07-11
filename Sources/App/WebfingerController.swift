760aa8e018c6562c063c878091fd2ef4a01b0a85
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-07-11 07:58:23 +0300

    webfinger user extraction

--- Sources/App/WebfingerController.swift
+++ Sources/App/WebfingerController.swift
@@ -1,14 +1,14 @@
 import Hummingbird
 
-struct WebfingerResourceDescriptor : ResponseEncodable {
-	struct Link : ResponseEncodable {
+struct WebfingerResourceDescriptor: ResponseEncodable {
+	struct Link: ResponseEncodable {
 		let rel: String
 		let type: String
 		let href: String
 	}
 
 	let subject: String
-	let aliases: [String]
+	let aliases: [String]?
 	let links: [Link]
 }
 
@@ -18,9 +18,35 @@ struct WebfingerController {
 			.get("/", use: get)
 	}
 
-	func get(request: Request, context: some RequestContext) async throws -> String? {
-		let value = request.uri.queryParameters["resource"] ?? "hmmm"
+	func get(request: Request, context: some RequestContext) async throws -> Response {
+		guard let value = request.uri.queryParameters["resource"]?.split(separator: "acct:").last else {
+			return Response(status: .badRequest)
+		}
 
-		return "hello: \(value)"
+		let components = value.split(separator: "@")
+		guard components.count == 2 else {
+			return Response(status: .badRequest)
+		}
+
+		let user = components[0]
+		let server = components[1]
+
+		let descriptor = WebfingerResourceDescriptor(
+			subject: String(user),
+			aliases: nil,
+			links: [
+				WebfingerResourceDescriptor.Link(
+					rel: "self",
+					type: "application/activity+json",
+					href: "http://127.0.0.1:8080/users/\(value)"
+				)
+			]
+		)
+
+		var response = try context.responseEncoder.encode(descriptor, from: request, context: context)
+
+		response.headers[.contentType] = "application/jrd+json"
+
+		return response
 	}
 }
